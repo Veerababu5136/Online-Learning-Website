942eb08ed1bb0ee1df2caae8c6c48f938bef21fe
veerababu12 <vinuvera1234@gmail.com>
2024-05-30 16:07:38 +0530

    first commit

---
+++ validate.js
@@ -0,0 +1,14 @@
+
+function validate()
+
+{
+
+    // Get the email and password values from the form
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    alert(email+password);
+
+   
+
+}
